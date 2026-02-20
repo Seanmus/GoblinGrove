6372f150c797cc2802f8613d03464900bcdececd
Sean Piche <seanpiche718@gmail.com>
2026-02-20 15:08:50 -0600

    Made movement locked to grid gameboy style

--- Player/goblin_farmer.gd
+++ Player/goblin_farmer.gd
@@ -1,21 +1,40 @@
 extends CharacterBody2D
 
-
-const SPEED = 25
+var animation_speed = 3
+var moving = false
+var tile_size = 16
+var last_input
+@export var ray: RayCast2D
 
 
 func _physics_process(delta: float) -> void:
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir = Input.get_vector("Left", "Right", "Up", "Down")
-	var direction = input_dir.normalized()
-	$AnimationTree.set("parameters/blend_position", input_dir)
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.y = direction.y * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.y = move_toward(velocity.y, 0, SPEED)
+	if(input_dir.length() > 0):
+		if moving:
+			return
+		else:
+			last_input = input_dir
+			$AnimationTree.set("parameters/blend_position", last_input)
+			if(abs(input_dir.x) > abs(input_dir.y)):
+				_move(Vector2(input_dir.x, 0).normalized())
+			else:
+				_move(Vector2(0, input_dir.y).normalized())
 
-	move_and_slide()
+	else:
+		last_input = input_dir
+		if !moving:
+			$AnimationTree.set("parameters/blend_position", last_input)
+		
+
+func _move(input_dir):
+	ray.target_position = input_dir * tile_size
+	ray.force_raycast_update()
+	if !ray.is_colliding():
+		var tween = create_tween()
+		tween.tween_property(self, "position",
+	   	position + 
+		input_dir * tile_size, 1.0/animation_speed).set_trans(Tween.TRANS_SINE)
+		moving = true
+		await tween.finished
+		moving = false
+	
