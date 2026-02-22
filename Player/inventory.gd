1d870f8b4f938de4c02adbb8696a4e523077438f
Sean Piche <seanpiche718@gmail.com>
2026-02-22 11:20:08 -0600

    Inventory now adds items dynamically

--- Player/inventory.gd
+++ Player/inventory.gd
@@ -18,9 +18,20 @@ func _ready() -> void:
 func _addItem(itemId):
 	#use a for int i loop to go through and find the next open slot
 	#maybe start at 6?
-	print("adding item " + str(Items.items[itemId].description))
-	inventory.get_or_add(6, inventorySlot.new(Items.items[itemId], 1))
-	emit_signal("inventoryUpdated")
+	for i in range(1, 23):
+		if inventory.has(i):
+			if(inventory[i].item.itemId == itemId):
+				inventory[i].count += 1
+				emit_signal("inventoryUpdated")
+				return
+	#if no match is found do add to next open slot
+	for i in range(1, 24):
+		if !inventory.has(i):
+			print("adding item " + str(Items.items[itemId].description))
+			inventory.get_or_add(i, inventorySlot.new(Items.items[itemId], 1))
+			emit_signal("inventoryUpdated")
+			return
+	
 
 func _useItem(itemSlot):
 	inventory[itemSlot].count -= 1
