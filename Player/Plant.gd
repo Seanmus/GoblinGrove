23d1c3175eb841e48624e7bfd3d00bfb80f16858
Sean Piche <seanpiche718@gmail.com>
2026-02-23 23:55:42 -0600

    Fixed bug with harvesting was related to delete being on resource which is no longer the parent class

--- Player/Plant.gd
+++ Player/Plant.gd
@@ -6,7 +6,7 @@ class_name Plant
 @export var tilePos = 1
 @export var stage = 1
 signal newStage(sprite)
-
+signal harvested
 func _set_stage(Stage):
 	stage = Stage
 	_set_sprite_for_stage(stage)
@@ -17,8 +17,9 @@ func _set_sprite_for_stage(stage):
 func _can_harvest():
 	if stage >= maxState:
 		Inventory._addItem(itemId)
-		call_deferred("queue_free")
+		harvested.emit()
 		return true
+
 
 	else:
 		return false
--- Player/plant_sprite.gd
+++ Player/plant_sprite.gd
@@ -5,6 +5,11 @@ extends Node2D
 func _ready() -> void:
 	plant._set_stage(plant.stage)
 	plant.newStage.connect(_on_plant_new_stage)
+	plant.harvested.connect(_Harvested)
 
 func _on_plant_new_stage(sprite: Variant) -> void:
 	$PlantSprite.texture = sprite
+
+
+func _Harvested():
+	call_deferred("queue_free")
